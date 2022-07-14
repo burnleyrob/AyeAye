03e8ebe7f79ea1d013dbc1a881b720471cbe7771
burnleyrob <robert.anker@outlook.com>
2022-07-14 15:40:40 +0100

    uncommented unittests

--- tests/test_pinnate.py
+++ tests/test_pinnate.py
@@ -5,26 +5,26 @@ from ayeaye.pinnate import Pinnate
 
 
 class TestPinnate(unittest.TestCase):
-    # def test_attrib_and_dict(self):
-    #     a = Pinnate({'my_string': 'abcdef'})
-    #     self.assertEqual(a.my_string, 'abcdef')
-    #     self.assertEqual(a['my_string'], 'abcdef')
-    #     self.assertEqual(a.as_dict(), {'my_string': 'abcdef'})
+    def test_attrib_and_dict(self):
+        a = Pinnate({'my_string': 'abcdef'})
+        self.assertEqual(a.my_string, 'abcdef')
+        self.assertEqual(a['my_string'], 'abcdef')
+        self.assertEqual(a.as_dict(), {'my_string': 'abcdef'})
 
-    # def test_recurse(self):
-    #     d = {'my_things': [1, 2, {'three': 3}]}
-    #     a = Pinnate(d)
-    #     p = a.my_things
-    #     self.assertTrue(p[0] == 1 and p[1] == 2 and isinstance(p[2], Pinnate))
-    #     self.assertEqual(p[2].three, 3)
+    def test_recurse(self):
+        d = {'my_things': [1, 2, {'three': 3}]}
+        a = Pinnate(d)
+        p = a.my_things
+        self.assertTrue(p[0] == 1 and p[1] == 2 and isinstance(p[2], Pinnate))
+        self.assertEqual(p[2].three, 3)
 
-    # def test_variable_method_name(self):
-    #     """
-    #     check it's possible to give a variable the same name as an existing method.
-    #     """
-    #     d = {'as_dict': 1}
-    #     p = Pinnate(d)
-    #     self.assertEqual("{'as_dict': 1}", str(p.as_dict()))
+    def test_variable_method_name(self):
+        """
+        check it's possible to give a variable the same name as an existing method.
+        """
+        d = {'as_dict': 1}
+        p = Pinnate(d)
+        self.assertEqual("{'as_dict': 1}", str(p.as_dict()))
 
     def test_as_json(self):
         d = {'number': 1,
@@ -46,48 +46,48 @@ class TestPinnate(unittest.TestCase):
 
         
 
-    # def test_recursive_lists(self):
-    #     "bug found with list inside a list"
+    def test_recursive_lists(self):
+        "bug found with list inside a list"
 
-    #     d = {'name': 'Stall Street',
-    #          'type': 'Line',
-    #          'payload': [[-2.3597675, 51.3797509], [-2.359739, 51.3797783]],
-    #          'tags': {'name': 'Stall Street',
-    #                   'oneway': 'yes',
-    #                   'highway': 'unclassified',
-    #                   'motor_vehicle:conditional': 'no @ (10:00-18:00)'
-    #                   }
-    #          }
-    #     p = Pinnate(d)
-    #     # accessing the item triggered the bug
-    #     self.assertEqual(-2.3597675, p.payload[0][0])
+        d = {'name': 'Stall Street',
+             'type': 'Line',
+             'payload': [[-2.3597675, 51.3797509], [-2.359739, 51.3797783]],
+             'tags': {'name': 'Stall Street',
+                      'oneway': 'yes',
+                      'highway': 'unclassified',
+                      'motor_vehicle:conditional': 'no @ (10:00-18:00)'
+                      }
+             }
+        p = Pinnate(d)
+        # accessing the item triggered the bug
+        self.assertEqual(-2.3597675, p.payload[0][0])
 
 
-    #     # deeper recurse
-    #     d = {'a': [[[{'b':'hello'}]]]}
-    #     p = Pinnate(d)
-    #     self.assertEqual('hello', p.a[0][0][0].b)
+        # deeper recurse
+        d = {'a': [[[{'b':'hello'}]]]}
+        p = Pinnate(d)
+        self.assertEqual('hello', p.a[0][0][0].b)
 
-    # def test_top_level_list(self):
-    #     "test you can make a Pinnate from a top level list"
+    def test_top_level_list(self):
+        "test you can make a Pinnate from a top level list"
         
-    #     l = ["a", "b", "c"]
+        l = ["a", "b", "c"]
 
-    #     p = Pinnate(l)
+        p = Pinnate(l)
 
-    #     self.assertEqual(p[0], "a")
-    #     self.assertEqual(p[1], "b")
-    #     self.assertEqual(p[2], "c")
+        self.assertEqual(p[0], "a")
+        self.assertEqual(p[1], "b")
+        self.assertEqual(p[2], "c")
 
-    # def test_top_level_list_of_dict(self):
-    #     "test you can make a Pinnate from a top level list of dictionaries"
+    def test_top_level_list_of_dict(self):
+        "test you can make a Pinnate from a top level list of dictionaries"
         
-    #     l = [{"first_name": "mary"}, {"last_name": "poppins"}]
+        l = [{"first_name": "mary"}, {"last_name": "poppins"}]
 
-    #     p = Pinnate(l)
+        p = Pinnate(l)
 
-    #     self.assertEqual(p[0].first_name, "mary")
-    #     self.assertEqual(p[1].last_name, "poppins")
+        self.assertEqual(p[0].first_name, "mary")
+        self.assertEqual(p[1].last_name, "poppins")
         
 
 if __name__ == "__main__":
