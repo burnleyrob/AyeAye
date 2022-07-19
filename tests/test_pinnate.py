cd538828cfc70f171636f85009dca8f880429d4a
Si Parker <si@plogic.co.uk>
2022-07-19 09:59:01 +0100

    added: black formatting added: a unittest for expected behaviour

--- tests/test_pinnate.py
+++ tests/test_pinnate.py
@@ -6,13 +6,13 @@ from ayeaye.pinnate import Pinnate
 
 class TestPinnate(unittest.TestCase):
     def test_attrib_and_dict(self):
-        a = Pinnate({'my_string': 'abcdef'})
-        self.assertEqual(a.my_string, 'abcdef')
-        self.assertEqual(a['my_string'], 'abcdef')
-        self.assertEqual(a.as_dict(), {'my_string': 'abcdef'})
+        a = Pinnate({"my_string": "abcdef"})
+        self.assertEqual(a.my_string, "abcdef")
+        self.assertEqual(a["my_string"], "abcdef")
+        self.assertEqual(a.as_dict(), {"my_string": "abcdef"})
 
     def test_recurse(self):
-        d = {'my_things': [1, 2, {'three': 3}]}
+        d = {"my_things": [1, 2, {"three": 3}]}
         a = Pinnate(d)
         p = a.my_things
         self.assertTrue(p[0] == 1 and p[1] == 2 and isinstance(p[2], Pinnate))
@@ -22,50 +22,52 @@ class TestPinnate(unittest.TestCase):
         """
         check it's possible to give a variable the same name as an existing method.
         """
-        d = {'as_dict': 1}
+        d = {"as_dict": 1}
         p = Pinnate(d)
         self.assertEqual("{'as_dict': 1}", str(p.as_dict()))
 
     def test_as_json(self):
-        d = {'number': 1,
-             'string': 'hello',
-             'date': datetime.strptime("2020-01-15 10:34:12", "%Y-%m-%d %H:%M:%S"),
-             'recurse_list': [{'abc': 'def'}],
-             'recurse_dict': {'ghi': {'jkl': 'mno'}
-                              },
-             }
+        d = {
+            "number": 1,
+            "string": "hello",
+            "date": datetime.strptime("2020-01-15 10:34:12", "%Y-%m-%d %H:%M:%S"),
+            "recurse_list": [{"abc": "def"}],
+            "recurse_dict": {"ghi": {"jkl": "mno"}},
+        }
         p = Pinnate(d)
         as_json = str(p.as_json())
-        expected = ('{"number": 1, "string": "hello", "date": "2020-01-15 10:34:12", '
-                    '"recurse_list": [{"abc": "def"}], "recurse_dict": {"ghi": {"jkl": "mno"}}}'
-                    )
+        expected = (
+            '{"number": 1, "string": "hello", "date": "2020-01-15 10:34:12", '
+            '"recurse_list": [{"abc": "def"}], "recurse_dict": {"ghi": {"jkl": "mno"}}}'
+        )
         self.assertEqual(expected, as_json)
 
     def test_recursive_lists(self):
         "bug found with list inside a list"
 
-        d = {'name': 'Stall Street',
-             'type': 'Line',
-             'payload': [[-2.3597675, 51.3797509], [-2.359739, 51.3797783]],
-             'tags': {'name': 'Stall Street',
-                      'oneway': 'yes',
-                      'highway': 'unclassified',
-                      'motor_vehicle:conditional': 'no @ (10:00-18:00)'
-                      }
-             }
+        d = {
+            "name": "Stall Street",
+            "type": "Line",
+            "payload": [[-2.3597675, 51.3797509], [-2.359739, 51.3797783]],
+            "tags": {
+                "name": "Stall Street",
+                "oneway": "yes",
+                "highway": "unclassified",
+                "motor_vehicle:conditional": "no @ (10:00-18:00)",
+            },
+        }
         p = Pinnate(d)
         # accessing the item triggered the bug
         self.assertEqual(-2.3597675, p.payload[0][0])
 
-
         # deeper recurse
-        d = {'a': [[[{'b':'hello'}]]]}
+        d = {"a": [[[{"b": "hello"}]]]}
         p = Pinnate(d)
-        self.assertEqual('hello', p.a[0][0][0].b)
+        self.assertEqual("hello", p.a[0][0][0].b)
 
     def test_top_level_list(self):
         "test you can make a Pinnate from a top level list"
-        
+
         l = ["a", "b", "c"]
 
         p = Pinnate(l)
@@ -76,7 +78,7 @@ class TestPinnate(unittest.TestCase):
 
     def test_top_level_list_of_dict(self):
         "test you can make a Pinnate from a top level list of dictionaries"
-        
+
         l = [{"first_name": "mary"}, {"last_name": "poppins"}]
 
         p = Pinnate(l)
@@ -89,7 +91,7 @@ class TestPinnate(unittest.TestCase):
 
         first_name = {"first_name": "mary"}
         p = Pinnate(first_name)
-        
+
         last_name = {"last_name": "poppins"}
         p.merge(last_name)
 
@@ -108,6 +110,18 @@ class TestPinnate(unittest.TestCase):
         self.assertEqual(p[0], 1)
         self.assertEqual(p[1], 2)
         self.assertEqual(p[2], 3)
+
+    def test_top_level_iterator(self):
+        "a list at the top level makes the Pinnate behave like a normal list"
+
+        p = Pinnate(["a", "b", "c"])
+        joined_values = "".join(p)
+
+        self.assertEqual("abc", joined_values)
+        self.assertFalse(isinstance(p, list), "It's a Pinnate, not a list")
+
+        px = Pinnate({"xx": ["a", "b", "c"]})
+        self.assertIsInstance(px.xx, list, "Second level really is a list")
 
 
 if __name__ == "__main__":
